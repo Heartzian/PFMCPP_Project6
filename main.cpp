632f67f2d8844b59c686c1c6a638236ea28f2eff
Heartzian <97336494+Heartzian@users.noreply.github.com>
2022-02-27 11:36:33 +0000

    Pointers practice Step by step

--- main.cpp
+++ main.cpp
@@ -24,40 +24,66 @@ Create a branch named Part2
  
  */
 
-
 #include <iostream>
 #include <string>
 struct T
 {
     int value; //2
-    std::string name; //3
-    T(int v, std::string n) : value(v), name(n) {}//1
+    int val;
+    int* ptrValue = &value;
+    int* ptrVal = &val;
+    const char* name; //3
+    //*name = valName;
+    T(int v, const char* n) : value(v), name(n) {std::cout << "T constructor starts!" << std::endl;}//1
+    ~T(){std::cout << "T destructor finish!" << std::endl;}
 };
 
 struct CheckPointer                                //4
 {
-    T* compare(T* a, T* b) //5
+    CheckPointer(){std::cout << "CheckPointer constructor starts!" << std::endl;}
+    ~CheckPointer(){std::cout << "CheckPointer destructor finish!" << std::endl;}
+    T* compare(T* addrTinst1, T* addrTinst2) //5
     {
-        if ( ( a != nullptr ) && ( b != nullptr) )
+        if ( ( addrTinst1 != nullptr ) && ( addrTinst2 != nullptr) )
         {
-            if( a->value < b->value ) return a;
-            if( a->value > b->value ) return b;
-        }
+            if( addrTinst1->value < addrTinst2->value ) 
+            {
+            	std::cout << "\taddrTinst1->value:\t" << addrTinst1->value << std::endl;
+                std::cout << "\taddrTinst1:\t\t" << addrTinst1 << std::endl;
+            	std::cout << "\taddrTinst2->value:\t" << addrTinst2->value << std::endl;
+                std::cout << "\taddrTinst2:\t\t" << addrTinst2 << std::endl;
+                return addrTinst1;
+            }
+            
+            if( addrTinst1->value > addrTinst2->value ) 
+            {
+            	std::cout << "\taddrTinst1->value:\t" << addrTinst1->value << std::endl;
+                std::cout << "\taddrTinst1:\t\t" << addrTinst1 << std::endl;
+            	std::cout << "\taddrTinst2->value:\t" << addrTinst2->value << std::endl;
+                std::cout << "\taddrTinst2:\t\t" << addrTinst2 << std::endl;
+                return addrTinst2;
+        	}
+        }   
         return nullptr;
     }
 };
 
 struct U
 {
+    U(){std::cout << "U constructor starts!" << std::endl;}
+    ~U(){std::cout << "U destructor finish!" << std::endl;}
     float uVariable1 { 0.0f }, uVariable2 { 0.0f };
-    //float* ptrTouVariable1 = &uVariable1;
-    //float* ptrTouVariable2 = &uVariable2;
+    float* ptrTouVariable1 = &uVariable1;
+    float* ptrTouVariable2 = &uVariable2;
+    
     float updateU(float* updatedValue)      //12
     {
         if( updatedValue != nullptr)
         {
             std::cout << "U's unit1 value: " << this->uVariable1 << std::endl;
             this->uVariable1 = *updatedValue;
+            std::cout << "\t'updatedValue':\t\t" << updatedValue << std::endl;
+            std::cout << "\t'*updatedValue':\t" << *updatedValue << std::endl;
             std::cout << "U's unit1 updated value: " << this->uVariable1 << std::endl;
             while( std::abs(this->uVariable2 - this->uVariable1) > 0.001f )
             {
@@ -77,21 +103,28 @@ struct U
 
 struct multiplyUVariables
 {
-    static float multiply(U* uInstance, float* newValue)         //10
+    multiplyUVariables(){std::cout << "multiplyUVariables constructor starts!" << std::endl;}
+    ~multiplyUVariables(){std::cout << "multiplyUVariables destructor finish!" << std::endl;}
+    static float multiply(U* uInstance, float* newUpdatedValue)         //10
     {
-        if(uInstance == nullptr || newValue == nullptr)
+        std::cout << "\n\t'U* uInstance':\t\t" << uInstance << std::endl;
+        if(uInstance == nullptr || newUpdatedValue == nullptr)
         {
             std::cout << "Warning: function arguments are nullptr" << std::endl;
             return 0.0f;
         }
         std::cout << "U's uVariable1 value: " << uInstance->uVariable1 << std::endl;
-        uInstance->uVariable1 = *newValue;
+        uInstance->uVariable1 = *newUpdatedValue;
+        std::cout << "\t'*newUpdatedValue':\t" << newUpdatedValue << std::endl;
+        std::cout << "\t'newUpdatedValue':\t" << *newUpdatedValue << std::endl;
         std::cout << "U's uVariable1 updated value: " << uInstance->uVariable1 << std::endl;
 
         while( std::abs(uInstance->uVariable2 - uInstance->uVariable1) > 0.001f )
         {
             if (uInstance->uVariable2 < uInstance->uVariable1)
             {
+                std::cout << "\t'uInstance->uVariable1':\t" << uInstance->uVariable1 << std::endl;
+                std::cout << "\t'uInstance->uVariable2':\t" << uInstance->uVariable2 << std::endl;
                 uInstance->uVariable2 += 0.1f;
                 std::cout << "uVariable2: " << uInstance->uVariable2 << std::endl;
             }
@@ -105,29 +138,26 @@ struct multiplyUVariables
         return uInstance->uVariable2 * uInstance->uVariable1;
     }
 }; 
-        
-/*
- MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
-
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
-    Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
-
- send me a DM to review your pull request when the project is ready for review.
-
- Wait for my code review.
- */
 
 int main()
 {
-    T tInst1( 1, "T struct instance 1" );               //6
-    T tInst2( 2, "T struct instance 1" );               //6
+    T tInst1( 3, "T struct instance 1" );//value y name NO han sido declarados
+    std::cout << "\t'tInst1.value':\t\t" << tInst1.value << std::endl;
+    std::cout << "\t'tInst1.ptrValue':\t" << tInst1.ptrValue << std::endl;
+    std::cout << "\t'tInst1.name':\t\t" <<tInst1.name << std::endl;
+    //std::cout << tInst1.valName << std::endl;
+    std::cout << "\t'&tInst1':\t\t" << &tInst1 << std::endl;
+    T tInst2( 2, "T struct instance 2" );               //6
+    std::cout << "\t'tInst2.value':\t\t" << tInst2.value << std::endl;
+    std::cout << "\t'tInst2.ptrValue':\t" << tInst2.ptrValue << std::endl;
+    std::cout << "\t'tInst2.name':\t\t" <<tInst2.name << std::endl;
+    std::cout << "\t'&tInst2':\t\t" << &tInst2 << std::endl;
     
     CheckPointer f;                                           //7
     
     auto* smaller = f.compare( &tInst1, &tInst2); 
+    std::cout << "\tsmaller:\t\t" << smaller << std::endl;
+
     if(smaller != nullptr) //8
     {
         std::cout << "the smaller one is << " << smaller->name << std::endl;
@@ -138,9 +168,19 @@ int main()
     }     //9
     
     U uInst1;
-    float updatedValue = 2.f;
+    std::cout << "\t'&uInst1':\t\t" << &uInst1 << std::endl;
+    std::cout << "\t'uInst1.uVariable1':\t" << uInst1.uVariable1 << std::endl;
+    std::cout << "\t'ptrTouVariable1':\t" << uInst1.ptrTouVariable1 << std::endl;
+    //std::cout << "'&uVariable1':\t\t" << &uVariable1 << std::endl;
+    std::cout << "\t'uInst1.uVariable2':\t" << uInst1.uVariable2 << std::endl;
+    std::cout << "\t'ptrTouVariable2':\t" << uInst1.ptrTouVariable2 << std::endl;
+    
+    float updatedValue = 0.5f;
+    std::cout << "\t'&updatedValue':\t" << &updatedValue << std::endl;
     std::cout << "\n[static func] uInst1's multiplied values: " << multiplyUVariables::multiply( &uInst1, &updatedValue ) << std::endl;                  //11
     
     U uInst2;
+    std::cout << "\t'&uInst2':\t\t" << &uInst2 << std::endl;
     std::cout << "\n[member func] uInst2's multiplied values: " << uInst2.updateU( &updatedValue ) << std::endl;
+    
 }
